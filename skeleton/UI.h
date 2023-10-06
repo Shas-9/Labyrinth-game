b0486d95e589700e1af8362cf4f36ab876cfd9d6
JoeBannouna <yo.m.hsm3@gmail.com>
2023-10-06 21:06:20 +1030

    slight restructuring

--- skeleton/UI.h
+++ skeleton/UI.h
@@ -2,19 +2,17 @@
 #define UI_INCLUDE
 
 #include <SFML/Graphics.hpp>
-#include <string>
 
-// #include "Game.h"
+#include "Game.h"
 #include "Vector.h"
-
 
 class UI {
  private:
   int highscores_num;
   int* highscores_values;
-  // string* highscores_names;
-  // string current_state;
-  // Game game;
+  string* highscores_names;
+  string current_state;
+  Game game;
   Vector screen_dimensions;
   sf::RenderWindow* window_ptr;
 
