d7abe32342c4ac8631fcd9d2c464c1a082ccda46
JoeBannouna <yo.m.hsm3@gmail.com>
2023-10-06 22:15:49 +1030

    finished unit test for Vector class

--- test/Vector.test.cpp
+++ test/Vector.test.cpp
@@ -4,10 +4,40 @@
 #include <criterion/logging.h>
 #include <stdio.h>
 
+// Tests whether the constructor with arguements sets attributes for the class
+// The getXPosition and getYPosition should return the corresponding values
 Test(vector_suite, test_1) {
   Vector vector(10, 10);
-  cr_log_info("THE VECTOR X VALUE IS: %f", vector.getXPosition());
 
   cr_assert(vector.getXPosition() == 10, "Test1: X Position Wrong.");
   cr_assert(vector.getYPosition() == 10, "Test1: Y Position Wrong.");
+}
+
+// Tests whether the default constructor sets both x & y attributes to zero
+Test(vector_suite, test_2) {
+  Vector vector;
+
+  cr_assert(vector.getXPosition() == 0, "Test2: X Position Wrong.");
+  cr_assert(vector.getYPosition() == 0, "Test2: Y Position Wrong.");
+}
+
+// Tests the setPosition method
+Test(vector_suite, test_3) {
+  Vector vector;
+
+  vector.setPosition(10, 15);
+
+  cr_assert(vector.getXPosition() == 10, "Test3: X Position Wrong.");
+  cr_assert(vector.getYPosition() == 15, "Test3: Y Position Wrong.");
+}
+
+// Tests the moveHorizontal & moveVertical methods
+Test(vector_suite, test_4) {
+  Vector vector;
+
+  vector.moveHorizontal(10);
+  vector.moveVertical(15);
+
+  cr_assert(vector.getXPosition() == 10, "Test4: X Position Wrong.");
+  cr_assert(vector.getYPosition() == 15, "Test4: Y Position Wrong.");
 }
