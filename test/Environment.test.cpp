e7813710bd04cab25610d848edea93f497107317
Shas-9 <xxshas9xx@gmail.com>
2023-10-17 23:26:31 +1030

    added some test files

--- test/Environment.test.cpp
+++ test/Environment.test.cpp
@@ -1,23 +1,74 @@
 #include "../src/Environment.h"
 
 #include <criterion/criterion.h>
-#include <criterion/logging.h>
-#include <stdio.h>
-#include <unistd.h>
 
-#include <iostream>
+// Test the constructor of the Environment class to ensure that
+// it correctly initializes the obstacles, items, and enemies.
+Test(environment_suite, test_constructor) {
+  sf::Texture obstacles_texture;
+  sf::Texture potion_texture;
+  sf::Texture iron_spider_texture;
 
-using std::cout;
-using std::endl;
+  Environment environment(&obstacles_texture, &potion_texture, &iron_spider_texture);
 
-Test(environment_suite, test_1) {
-  cout << "TEST IS RUNNING" << endl;
+  // Check that the number of obstacles, items, and enemies are greater than 0
+  cr_assert(environment.getObstaclesNum() > 0, "Obstacles not initialized.");
+  cr_assert(environment.getItemsNum() > 0, "Items not initialized.");
+  cr_assert(environment.getEnemiesNum() > 0, "Enemies not initialized.");
+}
 
-  // // Tests generation of 200 random mazes
-  // for (int i = 0; i < 200; i++) {
-  //   Environment* test_env = new Environment();
-  // }
+// Test that the getObstacles method returns a valid pointer.
+Test(environment_suite, test_get_obstacles) {
+  sf::Texture obstacles_texture;
+  sf::Texture potion_texture;
+  sf::Texture iron_spider_texture;
 
-  Environment* test_env = new Environment();
+  Environment environment(&obstacles_texture, &potion_texture, &iron_spider_texture);
 
+  Obstacle* obstacles = environment.getObstacles();
+
+  // Check that the returned pointer is not null
+  cr_assert_not_null(obstacles, "Obstacles pointer is null.");
+}
+
+// Test that the getItems method returns a valid pointer.
+Test(environment_suite, test_get_items) {
+  sf::Texture obstacles_texture;
+  sf::Texture potion_texture;
+  sf::Texture iron_spider_texture;
+
+  Environment environment(&obstacles_texture, &potion_texture, &iron_spider_texture);
+
+  Item* items = environment.getItems();
+
+  // Check that the returned pointer is not null
+  cr_assert_not_null(items, "Items pointer is null.");
+}
+
+// Test that the getEnemies method returns a valid pointer.
+Test(environment_suite, test_get_enemies) {
+  sf::Texture obstacles_texture;
+  sf::Texture potion_texture;
+  sf::Texture iron_spider_texture;
+
+  Environment environment(&obstacles_texture, &potion_texture, &iron_spider_texture);
+
+  Enemy* enemies = environment.getEnemies();
+
+  // Check that the returned pointer is not null
+  cr_assert_not_null(enemies, "Enemies pointer is null.");
+}
+
+// Test the getter methods for the number of obstacles, items, and enemies.
+Test(environment_suite, test_get_counts) {
+  sf::Texture obstacles_texture;
+  sf::Texture potion_texture;
+  sf::Texture iron_spider_texture;
+
+  Environment environment(&obstacles_texture, &potion_texture, &iron_spider_texture);
+
+  // Check that the number of obstacles, items, and enemies are greater than or equal to 0
+  cr_assert(environment.getObstaclesNum() >= 0, "Invalid number of obstacles.");
+  cr_assert(environment.getItemsNum() >= 0, "Invalid number of items.");
+  cr_assert(environment.getEnemiesNum() >= 0, "Invalid number of enemies.");
 }
--- test/MazeGenerator.test.cpp
+++ test/MazeGenerator.test.cpp
@@ -1,18 +1,36 @@
 #include "../src/MazeGenerator.h"
-
 #include <criterion/criterion.h>
 #include <criterion/logging.h>
-#include <stdio.h>
-#include <unistd.h>
 
-#include <iostream>
+// Test the generateMazePathConnections method to ensure it creates valid connections.
+Test(mazegenerator_suite, test_generate_maze_path_connections) {
+  sf::Texture obstacles_texture;
+  MazeGenerator maze_generator(&obstacles_texture);
 
-using std::cout;
-using std::endl;
+  PointConnections connections = maze_generator.generateMazePathConnections();
 
-Test(maze_generator_suite, test_1) {
-  cout << "TEST IS RUNNING" << endl;
+  cr_assert(connections.size() > 0, "No connections were generated.");
+}
 
-  MazeGenerator* generator = new MazeGenerator();
-  generator->generatePaths();
+// Test the computeGrid method to verify that it generates valid obstacles.
+Test(mazegenerator_suite, test_compute_grid) {
+  sf::Texture obstacles_texture;
+  MazeGenerator maze_generator(&obstacles_texture);
+  PointConnections connections = maze_generator.generateMazePathConnections();
+
+  Obstacle** obstacles = maze_generator.computeGrid(0, 0, MAZE_WIDTH, MAZE_BOX_SIZE, MAZE_BOX_THICKNESS, connections);
+
+  cr_assert_not_null(obstacles, "Obstacles array is not generated.");
+}
+
+// Test the getObstacles and getObstaclesNum methods.
+Test(mazegenerator_suite, test_get_obstacles_and_obstacles_num) {
+  sf::Texture obstacles_texture;
+  MazeGenerator maze_generator(&obstacles_texture);
+
+  Obstacle* obstacles = maze_generator.getObstacles();
+  int obstacles_num = maze_generator.getObstaclesNum();
+
+  cr_assert_not_null(obstacles, "Obstacles array is not generated.");
+  cr_assert(obstacles_num > 0, "Obstacles number is invalid.");
 }
