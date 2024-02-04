bd6a3bf7992ecba7ce857d0e8192dfe6390fbd3c
RajathaRanpatige <rajatharanpatige@gmail.com>
2024-02-04 19:50:28 +1030

    fixed clipping

--- src/FastPotion.cpp
+++ src/FastPotion.cpp
@@ -5,10 +5,10 @@ FastPotion::FastPotion() {}
 FastPotion::FastPotion(
   Vector position,
   sf::Texture* texture
-) : Item(position, Vector(45, 60), "fast", "a speed potion"), speedBoost(speedBoost) {
+) : Item(position, Vector(60, 45), "fast", "a speed potion"), speedBoost(speedBoost) {
 
   this->dimensions = Vector(60, 45);
-  sf::IntRect* rectSourceSprite = new sf::IntRect(4, 0, this->getDimensions().getX() / 2, this->getDimensions().getY() / 2);
+  sf::IntRect* rectSourceSprite = new sf::IntRect(4, 0, this->getDimensions().getX(), this->getDimensions().getY());
   sprite->setTexture(*texture);
   sprite->setTextureRect(*rectSourceSprite);
   sprite->scale(sf::Vector2f(2, 2));
