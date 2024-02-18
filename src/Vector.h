49139b7784a9e7737c20d01424caf934080688b9
JoeBannouna <yo.m.hsm3@gmail.com>
2024-02-18 14:40:38 +1030

    changed Vector class into struct

--- src/Vector.h
+++ src/Vector.h
@@ -6,23 +6,13 @@
 using std::cout;
 using std::endl;
 
-class Vector {
-private:
+struct Vector {
   double x;
   double y;
-
-public:
+  
   Vector();
   Vector(double x, double y);
   Vector(int x, int y);
-  void moveHorizontal(double value);
-  void moveVertical(double value);
-
-  double getX();
-  double getY();
-
-  void set(double x, double y);
-  void set(int x, int y);
 
   void print();
 };
